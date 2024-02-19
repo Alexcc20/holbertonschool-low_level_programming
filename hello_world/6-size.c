f49427db2f318f9c74f4c628fdff9bc76cfd7066
Alex <alexccv94@gmail.com>
2024-02-19 15:12:50 -0500

    Project

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,11 @@ float floatType;
 double doubleType;
 char charType;
 
-printf("Size of int: %ld bytes\n", sizeof(integerType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
 printf("Size of char: %ld byte\n", sizeof(charType));
-
+printf("Size of int: %ld bytes\n", sizeof(integerType));
+printf("Size of long int %zu bytes\n", sizeof(long int));
+printf("Size of long long int %zu bytes\n", sizeof(long long int));
+printf("Size of float %zu bytes\n", sizeof(float));
 return (0);
+
 }
