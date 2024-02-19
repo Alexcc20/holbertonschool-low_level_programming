bb523256968d70522a3ea31f21ce0076f00a89c0
Alex <alexccv94@gmail.com>
2024-02-19 12:29:27 -0500

    Project

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,7 @@
 int main(void) 
 {
 
-char str[50] = ("Programming is like building a multilingual puzzle");
-
-puts(str); 
-
+puts("Programming is like building a multilingual puzzle")
 return(0);
 
 }
