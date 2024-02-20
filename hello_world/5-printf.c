fca46c7800974ef3b388e12d8277c32af553ce7c
Alex <alexccv94@gmail.com>
2024-02-20 07:16:20 -0500

    Project

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-/**
-*print
-*the file
-*/
 
 int main(void)
 {
-
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
-
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
