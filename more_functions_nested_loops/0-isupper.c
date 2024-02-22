55868a4e1c015a3f33329c820a47ada8edbf4444
Alex <alexccv94@gmail.com>
2024-02-22 11:28:31 -0500

    Project

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * Returns 1 if c is uppercase - 0 otherwise
+ *
+ */
+int _isupper(int c);
+{
+	int c;
+
+	if ('C')
+	{
+	 return (1);
+	}
+
+	else
+	{
+	 return (0);
+	}
+}
