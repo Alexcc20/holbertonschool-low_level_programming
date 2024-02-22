08a3c2199be1c6298a303f0ecf4457a0d413a7ae
Alex <alexccv94@gmail.com>
2024-02-21 20:50:13 -0500

    Project

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,29 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - n is positive, negative or zero
- *
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
-		printf("%d is zero\n", n);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- functions_nested_loops/0-putchar.s
+++ functions_nested_loops/0-putchar.s
@@ -1,17 +1,9 @@
 	.file	"0-putchar.c"
 	.text
-	.section	.rodata
-.LC0:
-	.string	"%d is positive\n"
-.LC1:
-	.string	"%d is negative\n"
-.LC2:
-	.string	"%d is zero\n"
-	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB6:
+.LFB0:
 	.cfi_startproc
 	endbr64
 	pushq	%rbp
@@ -19,44 +11,39 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	$0, %edi
-	call	time@PLT
-	movl	%eax, %edi
-	call	srand@PLT
-	call	rand@PLT
-	subl	$1073741823, %eax
-	movl	%eax, -4(%rbp)
-	cmpl	$0, -4(%rbp)
-	jle	.L2
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC0(%rip), %rdi
+	movl	$95, %edi
 	movl	$0, %eax
-	call	printf@PLT
-	jmp	.L3
-.L2:
-	cmpl	$0, -4(%rbp)
-	jns	.L4
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC1(%rip), %rdi
+	call	_putchar@PLT
+	movl	$112, %edi
 	movl	$0, %eax
-	call	printf@PLT
-	jmp	.L3
-.L4:
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC2(%rip), %rdi
+	call	_putchar@PLT
+	movl	$117, %edi
 	movl	$0, %eax
-	call	printf@PLT
-.L3:
+	call	_putchar@PLT
+	movl	$116, %edi
 	movl	$0, %eax
-	leave
+	call	_putchar@PLT
+	movl	$99, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	movl	$104, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	movl	$97, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	movl	$114, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	movl	$10, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	movl	$0, %eax
+	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE6:
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
