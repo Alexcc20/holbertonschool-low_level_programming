f916960b932eb9ca309196222fdefb3d46314fcd
Alex <alexccv94@gmail.com>
2024-02-21 20:06:30 -0500

    Project

--- functions_nested_loops/0-putchar.s
+++ functions_nested_loops/0-putchar.s
@@ -1,9 +1,17 @@
 	.file	"0-putchar.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"%d is positive\n"
+.LC1:
+	.string	"%d is negative\n"
+.LC2:
+	.string	"%d is zero\n"
+	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB6:
 	.cfi_startproc
 	endbr64
 	pushq	%rbp
@@ -11,39 +19,44 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$95, %edi
+	subq	$16, %rsp
+	movl	$0, %edi
+	call	time@PLT
+	movl	%eax, %edi
+	call	srand@PLT
+	call	rand@PLT
+	subl	$1073741823, %eax
+	movl	%eax, -4(%rbp)
+	cmpl	$0, -4(%rbp)
+	jle	.L2
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
 	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$112, %edi
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	cmpl	$0, -4(%rbp)
+	jns	.L4
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC1(%rip), %rdi
 	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$117, %edi
+	call	printf@PLT
+	jmp	.L3
+.L4:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
 	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$116, %edi
+	call	printf@PLT
+.L3:
 	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$99, %edi
-	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$104, %edi
-	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$97, %edi
-	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$114, %edi
-	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$12142, %edi
-	movl	$0, %eax
-	call	_putchar@PLT
-	movl	$0, %eax
-	popq	%rbp
+	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE6:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
