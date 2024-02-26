ddacd3a394ef0f17657e581be981de4db7fc7b62
Alex <alexccv94@gmail.com>
2024-02-26 12:38:38 -0500

    Project

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * main - a function that takes a pointer and updates the value
  *
  * Return: Always 0.
  */
-int main(void)
+void reset_to_98(int *n)
 {
-	int n = 12;
-	int *pn = &n;
-
-	*pn = 10;
+*n = 98;
 }
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
