ce2f702197a27e2742e96530c042225175cf2793
Alex <alexccv94@gmail.com>
2024-02-25 21:41:36 -0500

    Project

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _itsalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
