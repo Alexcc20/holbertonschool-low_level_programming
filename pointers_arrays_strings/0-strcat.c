1d5bae7b5edf9dce71df62ebe4b87ad129623aaf
Alex <alexccv94@gmail.com>
2024-02-27 11:56:39 -0500

    Project

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * src - another array
  * _strcat - concatenates two strings
  */
-char *_strcat (char *dest, char *src);
+char *_strcat (char *dest, char *src)
 {
   char dest[100] = "hello";
   char src[100] = "world";
