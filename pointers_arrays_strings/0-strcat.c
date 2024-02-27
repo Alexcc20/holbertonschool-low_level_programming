7f266d85c65942378f3b2676eacccbab323a5078
Alex <alexccv94@gmail.com>
2024-02-27 10:22:12 -0500

    Project

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *dest - an array
- *src - another array
- *_strcat - concatenates two strings
+ * dest - an array
+ * src - another array
+ * _strcat - concatenates two strings
  */
-char *_strcat(char *dest, char *src);
+void _strcat (char *dest, char *src);
 {
-char dest[5];
-char src[5];
+  char dest[5];
+  char src[5];
 
-_srtcat(char dest, char src);
+  _strcat(dest, src);
 }
