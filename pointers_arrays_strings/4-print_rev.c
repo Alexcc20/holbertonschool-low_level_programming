c3829ef6f00ddde3952baeed34b69f05b4e3d88b
Alex <alexccv94@gmail.com>
2024-02-26 19:34:54 -0500

    Project

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,21 +6,5 @@
  */
 void print_rev(char *s)
 {
-	int i, j, len;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-
-	_putchar('\n');
+	print_rev(s);
 }
