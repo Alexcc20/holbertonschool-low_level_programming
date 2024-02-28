db1f87e9a495fa1bdbcf6a1bf6e0672b9a7fdd49
Alex <alexccv94@gmail.com>
2024-02-28 10:23:38 -0500

    Project

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies at most an inputted number
+ * dest - the buffer string the string copy
+ * src - the source string
+ * n - maximun number of bytes to be copied from src
+ * Return - a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	src_len = 0;
+
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = '\0';
+
+	return (dest);
+}
