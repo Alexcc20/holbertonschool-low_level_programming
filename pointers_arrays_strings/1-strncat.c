1b709861e1eb54c20cb5b891db1d2855743f0661
Alex <alexccv94@gmail.com>
2024-02-28 09:34:57 -0500

    Project

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * dest - a string
- * src - another string
+ * _strcat = concatenates two strings
+ * dest = the sting to be appended to dest
+ * src = the string to be appended to dest.
+ * n = The number of bytes from src to be appended to dest.
+ * Return = A pointer to the resulting string dest.
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int n = 10
-	int o = 5
+	int index = 0, dest_len = 0;
 
-
+	while (dest[index++])
+	dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+	dest[dest_len++] = src[index];
+	return (dest);
+}
