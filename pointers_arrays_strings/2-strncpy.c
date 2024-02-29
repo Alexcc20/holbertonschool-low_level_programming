1f30ff41bed2841d4f026bf6ab8c8a46f295bac2
Alex <alexccv94@gmail.com>
2024-02-29 00:16:26 -0500

    Project

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
- * _strncpy - copies at most an inputted number
- * dest - the buffer string the string copy
- * src - the source string
- * n - maximun number of bytes to be copied from src
- * Return - a pointer to the resulting string dest
+ * _strncpy - concatenate strings, defining the size of a second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * return: Always 0.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while ((*s1 != '\0\' && *s2 != '\0') && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else
-	{
-		return(*s1 - *s2);
-	}
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = (*src + i);
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
+	return (dest);
 }
