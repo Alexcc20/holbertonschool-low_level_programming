9b16c2faed85d18222afb210899837d08ff594d3
Alex <alexccv94@gmail.com>
2024-02-29 17:35:47 -0500

    Project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,3 +15,4 @@ void reverse_array(int *a, int n);
 int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
