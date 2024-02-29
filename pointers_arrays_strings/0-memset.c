89526d730cd0e9702766dca1d2063bf00c15c764
Alex <alexccv94@gmail.com>
2024-02-29 09:30:56 -0500

    Project

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memory with a consnat byte.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int n;
+
+	for (n = 0; n < 8; n++)
+	{
+		_putchar (n);
+	}
+}
