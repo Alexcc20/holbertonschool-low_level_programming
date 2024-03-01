5a458527db2f93917f4bc0a5279050dde706322b
Alex <alexccv94@gmail.com>
2024-02-29 21:55:27 -0500

    Project

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int counter1, counter2, < size: counter1++;
+	int counter1, counter2, sum1, sum2;
 
-	sun1 = 0;
-	sun2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 
 	for (counter1 = 0; counter1 < size; counter2++)
 	{
-		sun1 += a[(size + 1) * counter1];
+		sum1 += a[(size + 1) * counter1];
 	}
 	for (counter2 = 0; counter2 < size; counter2++)
 	{
-		sun2 += a[(size - 1) * (counter2 + 1)];
+		sum2 += a[(size - 1) * (counter2 + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
