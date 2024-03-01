c4e521452083d29ad1f4ca027c42ea730a111152
Alex <alexccv94@gmail.com>
2024-02-29 22:06:41 -0500

    Project

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,34 @@
 #include "main.h"
 /**
- * _strstr - locates a substring
- * @haystack: string to look though
- * @needle: substring to find
- * Return: pointer to string with result of search
- */
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int len;
-	/*Get length of needle for strncmp*/
-	len = 0;
-	while (needle[len] != '\0')
-		len++;
-	/*compare substring*/
-	while (*haystack != '\0')
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
 	{
-		if (_strncmp(haystack, needle, len) == 0)
-			return (haystack);
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
 		haystack++;
 	}
-	return (NULL);
+	return ('\0');
 }
