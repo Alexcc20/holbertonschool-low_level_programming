e1942bdbfcdca489c2e11cc19de46bd9419b49d7
Alex <alexccv94@gmail.com>
2024-03-01 12:48:38 -0500

    Project

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,23 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
- * main - Print all possible combinations of sinlge digit numbers
- *
- * Return: Always 0 (Success)
- */
+* main - Print numbers 0 to 9 separated by a comma
+*
+* Return: 0 for success
+*/
 int main(void)
 {
-	int number;
+	int digit;
 
-	for (number = 0; number <= 9; number++)
+	for (digit = 0; digit <= 9; digit++)
 	{
-	putchar(number + '0');
-	if (number < 9)
-	{
-	putchar(',');	
+		putchar(digit + '0');
+		if (digit != 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	}
-	putchar('n');
-
-return (0);
+	putchar('\n');
+	return (0);
 }
