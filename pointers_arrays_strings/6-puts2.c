c65b67c257437ff85c82cf7c1037170676a2d7ee
Alex <alexccv94@gmail.com>
2024-02-29 22:41:33 -0500

    Project

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,20 @@
 #include "main.h"
+
 /**
- * puts2 - function should print only one charator out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
 void puts2(char *str)
 {
-int i;
-int j = 0;
-
-while (str[j] != '\0')
+int i = 0;
+while (str[i] != '\0')
 {
-j++;
-}
-for (i = 0; i < j; i += 2)
-{
+if (i % 2 == 0)
 _putchar(str[i]);
+i++;
 }
+
 _putchar('\n');
 }
