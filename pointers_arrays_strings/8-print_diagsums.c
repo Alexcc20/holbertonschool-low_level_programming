1dfeb662465d06563fd935cbbbd99d0be5884b76
Alex <alexccv94@gmail.com>
2024-02-29 22:14:07 -0500

    Project

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_chessboard - entry point
- * @a: array
+ * print_diagsums - entry point
+ * @a: input
+ * @size: input
  * Return: always 0 (success)
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-int i, n;
+int i, n, sum1 = 0, sum2 = 0;
 
-for (i = 0; i < 8; i++)
-{
-for (n = 0; n < 8; n++)
-_putchar(a[i][n]);
-_putchar('\n');
-}
+for (i = 0; i <= (size * size); i = i + size + 1)
+sum1 = sum1 + a[i];
+
+for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+sum2 = sum2 + a[n];
+printf("%d, %d\n", sum1, sum2);
 }
