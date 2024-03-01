911c790f561bb733c07fe955f55535c16f117d0b
Alex <alexccv94@gmail.com>
2024-03-01 13:12:42 -0500

    Project

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98(int n)
- * followed by a new line
- * @n: print from this number
+ * print_to_98 - check the the entry to deteminate if is lower or upper
+ *
+ * @n: This is the entry
+ * Return: Always 0.
  */
+
 void print_to_98(int n)
 {
-	int i, j;
-
 	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
-		}
-	} else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
-	}
+		for (; n <= 97; n++)
+			printf("%d, ", n);
+	else
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
