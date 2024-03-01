57ef3980257e67344567d758262f6c9b4e9fa9b5
Alex <alexccv94@gmail.com>
2024-03-01 16:24:17 -0500

    Project

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
 /**
- * _strcat = concatenates two strings
- * dest = the sting to be appended to dest
- * src = the string to be appended to dest.
- * n = The number of bytes from src to be appended to dest.
- * Return = A pointer to the resulting string dest.
+ * _strncat - Write a function that concatenates two strings. it will use at
+ *            most n bytes from src
+ *
+ * @dest: This is the output  dest
+ * @src: This is the input src
+ * @n: This is the number bytes fro src
+ *
+ * Return: This is my return
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index, count;
 
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-	dest[dest_len++] = src[index];
+	for (index = 0; dest[index] != '\0'; index++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[index] = src[count];
+		index++;
+	}
+	if (count < n)
+	{
+	dest[index] = '\0';
+	}
 	return (dest);
 }
