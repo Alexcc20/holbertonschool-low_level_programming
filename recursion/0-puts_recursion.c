7e64a0201bb9326bf4e26ff53af0e9fcefff3a27
Alex <alexccv94@gmail.com>
2024-03-04 09:47:00 -0500

    Project

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * a function that prints a string followed by a new line
- */
-
+* _puts_recursion - prints a string followed by a new line
+*@s:character to be printed
+*Return:returns void
+*/
 void _puts_recursion(char *s)
 {
-	while (s <= "c")
+	if (*s == '\0')
+		_putchar('\n');
+	else
 	{
-		++s;
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
-
-	puts(s);
 }
