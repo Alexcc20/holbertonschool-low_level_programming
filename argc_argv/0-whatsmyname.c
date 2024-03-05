a6c211c78639aa2f9601650b8936236bc8419747
Alex <alexccv94@gmail.com>
2024-03-05 13:49:31 -0500

    Project

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main(int _attribute_((_unused_)) argc, char *argv[])
+/**
+* main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("/.%s/n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
