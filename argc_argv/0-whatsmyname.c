575ece29aec5bf94f473e1d2070a3bab6867dff2
Alex <alexccv94@gmail.com>
2024-03-05 13:47:15 -0500

    Project

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 
-int main(int argc, char *argv[])
+int main(int _attribute_((_unused_)) argc, char *argv[])
 {
-	printf("%s\".\n", argv[0]);
+	printf("%s/n", argv[0]);
 
 	return (0);
 }
