e917762fd93278f7dd848bd7c7b3303ebe66a3d6
Alex <alexccv94@gmail.com>
2024-03-26 23:53:00 -0400

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,15 +1,43 @@
-
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to head of list
- * @idx: index of the list where the new node should be added
- * @n: data of the new node
- * 
- * Return:  the address of the new node, or NULL if it failed
- */
-
+  * insert_dnodeint_at_index - inserts a node at an index
+  * @h: double pointer to a linked list
+  * @idx: where to add the node
+  * @n: value of the node
+  * Return: pointer to the new node
+  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-}#endif
+	dlistint_t *tmp, *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	tmp = *h;
+	if (!tmp)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = tmp;
+		tmp->prev = new;
+		*h = new;
+		return (new);
+	}
+	for ( ; idx > 1 && tmp->next; idx--)
+		tmp = tmp->next;
+	if (idx > 1 && !tmp->next)
+		return (NULL);
+	new->prev = tmp;
+	new->next = tmp->next ? tmp->next : NULL;
+	if (tmp->next)
+		tmp->next->prev = new;
+	tmp->next = tmp->next && idx > 1 ? NULL : new;
+	return (new);
+}
