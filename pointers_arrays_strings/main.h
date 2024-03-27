54cb4795fa0541c131d2ff22d2195b8d2da4039f
Alex <alexccv94@gmail.com>
2024-03-27 00:03:56 -0400

    test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,27 +1,23 @@
-char *_strcat(char *dest, char *src);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-int _atoi(char *s);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void puts2(char *str);
 char *_strcpy(char *dest, char *src);
-void puts2(char *str);
-char *cap_string(char *);
 int _putchar(char c);
-void print_array(int *a, int n);
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
